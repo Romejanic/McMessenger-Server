e6d22c6545d3386937c08292f2d8498fc1b42e8e
Romejanic <romejanic@gmail.com>
2018-06-20 11:11:46 +1000

    Http server again

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ const http = require("http");
 const port = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
-    res.write("<h1>Hello world!</h1>");
+    res.write("<h1>Hello world!</h1><p>" + req.url + "</p>");
     res.end();
 });
 
