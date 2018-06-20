9a39d41b912edb3cdeb68c5d427a30e7ddc348cd
Romejanic <romejanic@gmail.com>
2018-06-20 11:54:02 +1000

    Fixed static page directory

--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@ const http    = require("http");
 const app = express();
 const server = http.createServer(app);
 
-app.use(express.static("../www"));
+app.use(express.static("www"));
 
 const port = process.env.PORT || 8080;
 server.listen(port, () => {
-    console.log("Server started on localhost:", port);
+    console.log("Server started on localhost:" + port);
 });
