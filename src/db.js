6ca819afdf70b23a8e5fee9918901ad093676230
Jack Davenport <romejanic@gmail.com>
2019-08-06 11:17:18 +1000

    Fixed missing dev_config.json error on Heroku

--- src/db.js
+++ src/db.js
@@ -1,13 +1,17 @@
 const { Client } = require("pg");
-const devConfig = require("../dev_config.json");
+const fs = require("fs");
 
+// load configuration
+const devConfig = loadDevConfig();
 let connectionString = process.env.DATABASE_URL || devConfig.database_url;
 
+// create client object
 let client = new Client({
     connectionString: connectionString,
     ssl: connectionString !== devConfig.database_url
 });
 
+// connect to the database and initialize client
 async function init() {
     console.log("Attempting to connect to database...");
     await client.connect();
@@ -19,6 +23,16 @@ async function init() {
     });
 }
 
+// load developer config from file (if it exists)
+function loadDevConfig() {
+    if(fs.existsSync("../dev_config.json")) {
+        let json = fs.readFileSync("../dev_config.json");
+        return JSON.parse(json);
+    }
+    return {};
+}
+
+// export methods
 module.exports = {
     init
 };
