8ac1e4f356ed4652479c109eb5bf2dfa68680dd3
Jack Davenport <jack.davenport5011@gmail.com>
2020-10-03 23:17:41 +1000

    Added database init

--- src/db.js
+++ src/db.js
@@ -13,9 +13,16 @@ let client = new Client({
 
 // connect to the database and initialize client
 async function init() {
-    console.log("Attempting to connect to database...");
+    console.log("[DB] Attempting to connect to database...");
     await client.connect();
-    console.log("Connection established!");
+    console.log("[DB] Connection established!");
+    await initDatabase();
+}
+
+async function initDatabase() {
+    await client.query("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, uuid TEXT NOT NULL, status_code INT, status_data TEXT, show_hat BOOL)");
+    await client.query("CREATE TABLE IF NOT EXISTS messages (id INT UNIQUE PRIMARY KEY NOT NULL, content TEXT, sender INT, thread INT)");
+    await client.query("CREATE TABLE IF NOT EXISTS message_threads (user_a INT, user_b INT)");
 }
 
 // load developer config from file (if it exists)
